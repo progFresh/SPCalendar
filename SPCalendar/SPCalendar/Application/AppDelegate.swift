986a58963cfa7ef0820c0a61c485195573d0aca7
Сергей Полозов <fresher40@gmail.com>
2019-03-20 00:57:05 +0300

    Init project

---
+++ SPCalendar/SPCalendar/Application/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SPCalendar
+//
+//  Created by Сергей Полозов on 20/03/2019.
+//  Copyright © 2019 Сергей Полозов. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ SPCalendar/SPCalendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPCalendar
+//
+//  Created by Сергей Полозов on 20/03/2019.
+//  Copyright © 2019 Сергей Полозов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
